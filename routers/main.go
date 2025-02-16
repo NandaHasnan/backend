d393297e8e8d3c9f148a9c179d3dbe3fa5aec302
NandaHasnan <nandahasnan6@gmail.com>
2025-02-16 09:52:09 +0700

    pembaruan

--- routers/main.go
+++ routers/main.go
@@ -12,6 +12,7 @@ func Routers(router *gin.Engine) {
 	// 	ExposeHeaders:    []string{"Content-Length"},
 	// 	AllowCredentials: true,
 	// }))
+
 	router.Use(func(ctx *gin.Context) {
 
 		ctx.Header("Access-Control-Allow-Origin", "*")
@@ -25,7 +26,7 @@ func Routers(router *gin.Engine) {
 
 	router.Use(func(ctx *gin.Context) {
 		if ctx.Request.Method == "OPTIONS" {
-			ctx.Header("Access-Control-Allow-Origin", "http://localhost:5173")
+			ctx.Header("Access-Control-Allow-Origin", "https://fgh22-frontend.vercel.app")
 			ctx.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
 			ctx.Header("Access-Control-Allow-Headers", "Authorization, Content-Type")
 			ctx.Header("Access-Control-Allow-Credentials", "true")
